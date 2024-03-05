8180d84a56d253fc17f8a4efdcd7ab6f88ea68eb
Pedro Carvalho <125154226+PedroahCarvalho@users.noreply.github.com>
2024-03-04 21:53:38 -0300

    Atualização

--- EstruturaDeDadosII/busca_ibge.py
+++ EstruturaDeDadosII/busca_ibge.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
+from ibge import busca
 
 app = Flask(__name__)
 
 
 @app.route("/busca_nome")
-def busca():
+def busca_nome():
     try:
         var_nome = request.args.get("nome")
-        return f"{var_nome}"
+        response = busca(var_nome)
+        print(type(response))
+        # percorrer o retorno do busca e somar todas as ocorrências do nome
+        return response
     except Exception as e:
         return "Falha na rota /busca_nome: {e}"
-    pass
 
 
 app.run(debug=True)
